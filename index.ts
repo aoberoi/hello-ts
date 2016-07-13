dd186b3633c54cb5f1fbb70dd78f11ef1ceb3151
Ankur Oberoi <aoberoi@gmail.com>
2016-07-12 22:58:45 -0700

    improvements
    * fixes some stuff in package.json
    * adds nvmrc
    * adds tslint
    * adds an example of using an npm module

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import * as http from 'http';
 import * as message from './message';
+import * as log from 'loglevel';
 
 const server = http.createServer((request, response) => {
-  console.log('request received');
   response.statusCode = 200;
   response.end(message.myMessage);
 });
 
 server.listen(8080, () => {
-  console.log('listening on 8080');
+  log.info('listening on port 8080');
 });
